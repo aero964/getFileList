28c14ae49c47f285303bf9ca484aa657eca41fdf
aero964 <jsp1440@outlook.com>
2018-06-06 14:23:39 +0900

    コマンドラインでの結果出力を見やすく

--- getFileList.php
+++ getFileList.php
@@ -2,26 +2,30 @@
 
 //ファイルリストを取ってくる 
 
-$filelist = file_get_contents('http://www.xxxxxxxxxxx.ac.jp/~pwr/csv/All/');
+$filelist = file_get_contents('http://www.pu-kumamoto.ac.jp/~pwr/csv/All/');
 
 //テスト出力
 
-//echo $filelist;
-
 preg_match_all('/"[0-9]{8}\.csv"/', $filelist, $match);
 
+$getNumbers = 0;
 
 foreach ($match[0] as $key) {
-	$key = str_replace('"', '', $key);
-	//echo $key."<BR>";
+        $key = str_replace('"', '', $key);
 
-	//exsist check
-	if(!file_exists('./files/'.$key)){
-		$file = fopen('./files/'.$key ,"w");
-		fwrite($file, file_get_contents('http://www.xxxxxxxxxxx.ac.jp/~pwr/csv/All/'.$key));
-		fclose($file);
-	}
+        //exsist check
+        if(!file_exists('./files/'.$key)){
+                $file = fopen('./files/'.$key ,"w");
+                fwrite($file, $filelist);
+                fclose($file);
+                $getNumbers ++;
+        }
 }
 
-echo "OK";
-//print_r($match[0]);
+
+if(!$getNumbers){
+        echo "No file Added.".PHP_EOL;
+}else{
+        echo $getNumbers." File(s) Added.".PHP_EOL;
+}
+
